a578f3a0cd4036bb81c060f74060125d0ea41b6a
justindglee <nninnnin7@gmail.com>
2022-01-13 01:04:15 +0900

    Add user routes and cors settings

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,66 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const axios = require("axios");
+const User = require("../models/User");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router();
+
+router.get("/", async function (req, res, next) {
+  try {
+    const { kakaoId } = req.query;
+
+    const user = await User.findOne({ kakaoId });
+
+    console.log("그런 유저 있습니까..?", user);
+
+    if (user) {
+      res.cookie("user", user._id.toString(), {
+        httpOnly: false,
+        secure: true,
+        expires: new Date(Date.now() + 900000),
+        sameSite: "none",
+      });
+      res.send("유저 이써");
+      return;
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+
+    next(error);
+  }
+});
+
+// 가입시키기
+router.post("/", async function (req, res, next) {
+  try {
+    const { kakaoId, username } = req.body;
+
+    if (isNaN(kakaoId)) {
+      new Error("Invalid kakaoId");
+    }
+
+    const user = await User.findOne({ kakao_id: kakaoId });
+
+    // 이미 가입한 경우
+    if (user) {
+      res.json({
+        result: "failed",
+        message: "이미 가입한 유저입니다!",
+      });
+
+      return;
+    }
+
+    const newUser = new User({ kakao_id: kakaoId, name: username });
+    await newUser.save();
+
+    res.json({ result: "ok", newUser });
+  } catch (err) {
+    console.log(err);
+
+    next(err);
+  }
 });
 
 module.exports = router;
